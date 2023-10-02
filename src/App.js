ce8158c5da47228c062bfe9c493e6e960df7f1f5
TekleJankhoteli <jankhotelitekla@gmail.com>
2023-10-02 22:33:55 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {useState}from "react";
 import './App.css';
-import user1Icon from "./Avatar User2";
+import user1Icon from "./Avatar User2.svg";
+import user2Icon from "./Avatar User.png";
+import sendIcon from "./send.png"
 
 
 function App() {
+    // UseState and Functions
+
     const [messages, setMessages] = useState([]);
     const [inputUser1, setInputUser1] = useState("");
     const [inputUser2, setInputUser2] = useState("");
@@ -19,7 +23,7 @@ function App() {
     const user1Send = (e) => {
       e.preventDefault();
       const newMessage = {
-        user: "User 1",
+        user: "User1",
         text: inputUser1,
         timestamp: Date.now(),
       };
@@ -30,7 +34,7 @@ function App() {
     const user2Send = (e) => {
       e.preventDefault();
       const newMessage = {
-        user: "User 2",
+        user: "User2",
         text: inputUser2,
         timestamp: Date.now(),
       };
@@ -38,28 +42,43 @@ function App() {
       setInputUser2("");
     };
   
-    
+    // sort messages
     const sortedMessages = messages.slice().sort((a, b) => a.timestamp - b.timestamp);
   
     return (
+      <div className="app">
       <div className="container">
         <div className="chat">
-          <div className="output">
-            {sortedMessages.map((message, index) => (
-              <div className={`message ${message.user}`} key={index}>
-                {message.user}: {message.text}
-              </div>
-            ))}
-          </div>
+
+            {/* output */}
+
+        <div className="output">
+          {sortedMessages.map((message, index) => (
+            <div className={`message ${message.user}`} key={index}>
+              
+              {/* change user Icons */}
+
+              {message.user === "User1" ? (
+                <img src={user1Icon} alt="User1 Icon" className="user-icon" />
+              ) : (
+                <img src={user2Icon} alt="User2 Icon" className="user-icon" />
+              )}
+            <div className="message-text">   {message.text}</div>
+            </div>
+          ))}
+        </div>
+
+           {/* Input */}
+
           <div className="input">
             <form onSubmit={user1Send}>
               <h1>User 1</h1>
-             <img src={user1Icon} alt="" />
               <input
                 type="text"
                 onChange={user1InputChange}
                 value={inputUser1}
               />
+                <img src={sendIcon} alt="Icon" className="icon" />
             </form>
   
             <form onSubmit={user2Send}>
@@ -68,12 +87,16 @@ function App() {
                 type="text"
                 onChange={user2InputChange}
                 value={inputUser2}
+                
               />
+              <img src={sendIcon} alt="Icon" className="icon" />
             </form>
           </div>
         </div>
       </div>
+      </div>
     );
+    
   }
   
 export default App;
